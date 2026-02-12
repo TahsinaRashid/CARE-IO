c8713807684e9dd25ee7ab7eef89e1b9e95024c2
TahsinaRashid <tahsinarashid123@gmail.com>
2026-02-12 22:20:35 +0600

    Navbar & Footer

---
+++ src/app/layout.jsx
@@ -0,0 +1,45 @@
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/layouts/Navbar";
+import Footer from "@/components/layouts/Footer";
+
+const poppins=Poppins(
+  {
+    weight:["100","200","400","500","600","800"],
+  }
+)
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Care.XYZ",
+  description: "Baby Sitting & Elderly Care Service Platform",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${poppins.className} antialiased`}
+      >
+        <header className="py-2 md:w-11/12 mx-auto">
+          <Navbar/>
+        </header>
+        <main className="py-2 md:w-11/12 mx-auto">
+          {children}
+        </main>
+        <footer className="py-2 md:w-11/12 mx-auto">
+          <Footer/>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ src/components/layouts/Navbar.jsx
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import React from 'react'
+import NavLink from '../buttons/NavLink'
+
+export default function Navbar() {
+    const nav = (<>
+    <li className='flex justify-between items-center gap-5'>
+        <NavLink href={"/"}>Home</NavLink>
+        <NavLink href={"/services"}>Services</NavLink>
+        <NavLink href={"/about"}>About Us</NavLink>
+        <NavLink href={"/contact"}>Contact</NavLink>
+    </li>
+    </>
+    );
+  return (
+    <div>
+        <div className="navbar bg-base-100 shadow-2xl ">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+      </div>
+    <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        {nav}
+      </ul>
+    </div>
+    <Link href={"/"} className="btn btn-ghost text-xl">Care.IO</Link>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+   {nav}
+  </div>
+  <div className="navbar-end">
+    <Link href={"/login"} className="btn btn-primary">Login</Link>
+  </div>
+</div>
+    </div>
+  )
+}
